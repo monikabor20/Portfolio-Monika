76bc71f64d582cdb4a537c35974fc26e2f2c880d
Monika Borawska <monikabor20@wp.pl>
2018-03-15 15:39:21 +0100

    Zmiany w animacjach i przejsciach sekcji

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+$( window ).scroll(function() {
+        var wys = $(window).scrollTop();
+        if(wys > 600){
+            $('.navbar').css("background-color", "#b11414");
+            $('.nav-item a').css("color", "white");
+        } else {
+            $('#mainNav').css("background-color", "white");
+             $('.nav-item a').css("color", "#585353");
+        }
+    });
