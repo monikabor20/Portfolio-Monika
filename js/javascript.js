b0b32010498464812b6ef2a1a58d7cc8c1149c72
Monika Borawska <monikabor20@wp.pl>
2018-03-17 13:33:38 +0100

    dodanie animacji i polaczenie nawigacji z sekcjami

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,7 @@ $( window ).scroll(function changeNav () {
         if(wys > 600){
             $('.navbar').css("background-color", "#b11414");
             $('.nav-item a').css("color", "white");
+            
         } else {
             $('#mainNav').css("background-color", "white");
              $('.nav-item a').css("color", "#585353");
