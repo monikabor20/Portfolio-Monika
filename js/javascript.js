4d2cdacd0a16afd39178769c3c7a06688638327a
Monika Borawska <monikabor20@wp.pl>
2018-05-23 17:58:26 +0200

    Dodanie projektu

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,11 @@
-$( window ).scroll(function changeNav () {
-        var wys = $(window).scrollTop();
-        if(wys > 500){
-            $('.navbar').css("background-color", "#b11414");
-            $('.nav-item a').css("color", "white");
-            
-        } else {
-            $('#mainNav').css("background-color", "white");
-             $('.nav-item a').css("color", "#585353");
-        }
-    });
+$(window).scroll(function changeNav() {
+    var wys = $(window).scrollTop();
+    if (wys > 500) {
+        $('.navbar').css("background-color", "#b11414");
+        $('.nav-item a').css("color", "white");
 
+    } else {
+        $('#mainNav').css("background-color", "white");
+        $('.nav-item a').css("color", "#585353");
+    }
+});
